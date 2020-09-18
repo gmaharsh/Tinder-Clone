16cd4aa5da3e002f21eb02b1be993984796cf5b6
Maharsh Gheewala <maharshgheewala@dhcp37.merusouth114.iit.edu>
2020-09-18 10:53:54 -0500

    Header added

---
+++ tinder/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="app_body">
+
+        <Header />
+
+        {/* BODY */}
+        {/* SWIPR BUTTON */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ tinder/src/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Header.css';
+import PersonIcon from '@material-ui/icons/Person';
+import MessageIcon from '@material-ui/icons/Forum'
+import IconButton from  '@material-ui/core/IconButton'
+
+function Header() {
+    return (
+        <div className="header">
+            <IconButton>
+                <PersonIcon fontSize="large" className="header_icon" />
+            </IconButton>
+            <img className="header_logo" src="https://1000logos.net/wp-content/uploads/2018/07/tinder-logo-768x432.png" alt="Tinder_Icon" />
+            <IconButton>
+                <MessageIcon />
+            </IconButton>
+        </div>
+        
+    )
+}
+
+export default Header
